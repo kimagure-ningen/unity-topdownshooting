9922193e672bfdf166d7aae081deebf057bbbc1a
Daito <daitoxyamagishi@gmail.com>
2022-01-17 10:59:57 +0900

    Area (Have to think about timers)

--- Shooting Game/Assets/Scripts/AreaDamage.cs
+++ Shooting Game/Assets/Scripts/AreaDamage.cs
@@ -5,10 +5,8 @@ public class AreaDamage : MonoBehaviour
 {
     void OnTriggerStay(Collider other)
     {
-        Debug.Log("Hit!");
         if (other.gameObject.tag == "Enemy")
         {
-            Debug.Log("Enemy Hit!");
             other.gameObject.GetComponent<Enemy>().TakeDamage(10f * Time.deltaTime);
         }
     }
